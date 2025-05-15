5608df5b1e65117fd5f99d748c963844667a3b52
Hanumansai72 <hanumansai72@gmail.com>
2025-05-15 13:16:43 +0530

    vendor backend api updated

--- api/app.js
+++ api/app.js
@@ -179,10 +179,10 @@ app.get("/:id/settings",async(req,res)=>{
     const vendorsid=  req.params.id;
     const datasettings= await database.findById(vendorsid)
     res.json({datasettings})
+    console.log(datasettings)
   }
   catch(err){
     res.json("got an error",err)
-
 
   }
 })
