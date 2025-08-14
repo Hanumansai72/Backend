8aadebe662b27a97a1784aa6c5d94f0be6e5ce7b
Hanumansai72 <hanumansai72@gmail.com>
2025-08-14 19:58:27 +0530

    Add vendor and product navbar ssconents

--- api/app.js
+++ api/app.js
@@ -683,7 +683,6 @@ app.get("/myprofile/:id",async (req,res)=>{
 
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('./cloudinaryConfig');
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
