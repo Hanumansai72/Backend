5de8aeecedc416f9300640ead6d81436e5587aef
Hanumansai72 <hanumansai72@gmail.com>
2025-08-25 19:44:30 +0530

    process.en cloudarny update

--- api/app.js
+++ api/app.js
@@ -27,8 +27,8 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: "dqxsgmf33",
-  api_key: process.env.API_KEY_CLOUDNARY,
-  api_secret: process.env.Api_secret
+  api_key: "188635975854673",
+  api_secret:"XT91-J2eY6-G7TFFlwAiwglYPiQ"
 });
 
 
