c16912f6e537dead441d217ce09ecba05680df32
Hanumansai72 <hanumansai72@gmail.com>
2025-12-30 14:27:22 +0530

    npm install v3

--- api/app.js
+++ api/app.js
@@ -44,7 +44,8 @@ app.use(cors({
   origin: process.env.ALLOWED_ORIGINS?.split(',').map(o => o.trim()) || ['http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
 }));
 
 // Security headers
